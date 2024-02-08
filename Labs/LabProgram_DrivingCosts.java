301f701c39ae4992be4d78619fc283650b532de9
Bigg-Iron <19870186+Bigg-Iron@users.noreply.github.com>
2024-02-07 22:33:47 -0800

    Finished Lab for Driving Costs

---
+++ Labs/LabProgram_DrivingCosts.java
@@ -0,0 +1,29 @@
+package Labs;
+
+import java.util.Scanner;
+
+public class LabProgram_DrivingCosts {
+    public static void main(String[] args) {
+        Scanner scnr = new Scanner(System.in);
+
+        // System.out.println("Enter car's gas mileage ( miles/gallon): ");
+        double mileage = scnr.nextDouble();
+
+        // System.out.println("Enter the cost of gas (dollars/gallon): ");
+        double costPerGallon = scnr.nextDouble();
+
+        // Calculate gas cost for 20 miles
+        double gasCost20Miles = (20.0 / mileage) * costPerGallon;
+
+        // Calculate gas cost for 75 miles
+        double gasCost75Miles = (75.0 / mileage) *costPerGallon;
+
+        // Calculate gas cost for 75 miles
+        double gasCost500Miles = (500.0 / mileage) *costPerGallon;
+
+        // Output the results with two digits after the decimal point
+        System.out.printf("%.2f%n %.2f%n %.2f%n", gasCost20Miles,gasCost75Miles,gasCost500Miles);
+
+        
+    }
+}
